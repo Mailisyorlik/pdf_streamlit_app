63e4d03bc1025dffde4aceb686f00b98310e64e6
Liam Kilroy <liamkilroy@gmail.com>
2022-03-29 10:57:45 -0700

    Update pdf_streamlit_app.py

--- pdf_streamlit_app.py
+++ pdf_streamlit_app.py
@@ -1,4 +1,22 @@
 #initial streamlit file
 import streamlit as st
+from pdfminer.high_level import extract_text
+import tkinter.filedialog
+from 
 
-st.write("hello world")
+
+pdf_file = tkinter.filedialog.askopenfilename()
+#precompile regex patterns here
+
+
+#may need a directory specification
+text = extract_text(pdf_file)
+#print(type(text))
+#with open('training.txt', 'w') as f:
+   # f.writelines(text.join(text))
+
+print(text)
+
+
+
+st.write(text)
